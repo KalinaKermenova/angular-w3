069b411638935639cc0f0b667c1f4f5e7c2e49d3
KalinaKermenova <kalina.kermenova@gmail.com>
2024-05-09 18:25:13 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+ 
+type Note = {
+  title: string;
+  content: string;
+};
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-w3';
+  title = 'Тефтер-шън';
+ 
+  public newNote: Note = { title: '', content: '' };
+  public notes: Note[] = [];
+  public selectedNoteIndex: number = -1;
+  private isNewNote: boolean = true;
+ 
+  @ViewChild('notesForm') notesForm!: NgForm;
+ 
+  public saveNote() {
+    this.newNote = {
+      title: this.newNote.title,
+      content: this.newNote.content,
+    };
+ 
+    if (this.isNewNote) {
+      this.notes.push(this.newNote);
+    } else {
+      this.notes[this.selectedNoteIndex] = this.newNote;
+      this.isNewNote = true;
+    }
+ 
+    this.newNote = { title: '', content: '' };
+    this.notesForm.resetForm();
+  }
+ 
+  public editNote(index: number) {
+    this.newNote = { ...this.notes[index] };
+    this.selectedNoteIndex = index;
+    this.isNewNote = false;
+  }
+ 
+  public deleteNote(index: number) {
+    this.notes.splice(index, 1);
+    if (this.selectedNoteIndex === index) {
+      this.newNote = { title: '', content: '' };
+      this.selectedNoteIndex = -1;
+    }
+  }
+ 
+  public selectNote(index: number) {
+    this.selectedNoteIndex = index;
+  }
 }
